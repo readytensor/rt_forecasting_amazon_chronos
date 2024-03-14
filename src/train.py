cef1df6a05898d5aa561e94cc696b4602d3b0752
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-03-14 22:26:51 +0200

    updated docker file

--- src/train.py
+++ src/train.py
@@ -1,8 +1,15 @@
+import os
+from config import paths
+
+
 def run_training() -> None:
     """
     No-Op
     """
-    return
+    dummy_file_path = os.path.join(paths.MODEL_ARTIFACTS_PATH, "dummy.txt")
+    os.makedirs(paths.MODEL_ARTIFACTS_PATH, exist_ok=True)
+    with open(dummy_file_path, "w") as file:
+        file.write("model artifacts")
 
 
 if __name__ == "__main__":
