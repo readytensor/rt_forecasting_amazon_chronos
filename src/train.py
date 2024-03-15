d03d3655493f3ee70edf46a9748b06dda4beaafb
rt-desai <abhyuday.desai@readytensor.com>
2024-03-14 18:48:51 -0700

    add logging in train.py

--- src/train.py
+++ src/train.py
@@ -1,15 +1,19 @@
 import os
 from config import paths
+from logger import get_logger
 
+
+logger = get_logger(task_name="train")
 
 def run_training() -> None:
     """
     No-Op
     """
-    dummy_file_path = os.path.join(paths.MODEL_ARTIFACTS_PATH, "dummy.txt")
+    logger.info("This is a pretrained model, no training required.")
+    dummy_file_path = os.path.join(paths.MODEL_ARTIFACTS_PATH, "no_op.txt")
     os.makedirs(paths.MODEL_ARTIFACTS_PATH, exist_ok=True)
     with open(dummy_file_path, "w") as file:
-        file.write("model artifacts")
+        file.write("Pretrained LLM model. No training required.")
 
 
 if __name__ == "__main__":
